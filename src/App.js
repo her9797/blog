3da042ad1d9d1ede3a8fdc34396447d92ac9acea
her97 <simkoon0120@gmail.com>
2024-06-20 23:31:13 +0900

    blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Aside from './components/Aside';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div id="colorlib-page">
+            <Aside /> 
+            <div id="colorlib-main">
+            </div>
+        </div>
+    );
+};
 
 export default App;
